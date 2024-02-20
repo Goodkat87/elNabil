ec586c081781b08d002b07d1d59940c539128532
Goodkat87 <ilyass.zrl@hotmail.com>
2024-02-20 11:20:25 +0100

    Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 import { useState } from 'react'
 import data from "./assets/json/data.json"
 import Card from './components/Card'
+import Footer from './components/Footer'
+import Copyright from './components/Copyright'
 
 function App() {
 
   return (
     <div className='w-full h-screen flex flex-col items-center'>
-      <div className='w-11/12 h-max flex flex-wrap justify-center gap-3'>
+      <div className='w-11/12 h-max flex flex-wrap justify-center gap-3 pb-32'>
         {data.map(function(item) {
           return (
             <Card item={item}></Card>
           )
         })}
       </div>
+      <Footer></Footer>
+      <Copyright></Copyright>
     </div>
   )
 }
---
+++ src/components/Copyright.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Copyright() {
+  return (
+        <footer className="footer footer-center p-4 bg-base-200 text-base-content">
+            <aside>
+                <p>Copyright © 2024 - All right reserved by ACME Industries Ltd</p>
+            </aside>
+        </footer>
+  )
+}
