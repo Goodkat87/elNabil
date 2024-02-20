16563ee32fe3fb9f2984aa1cebde091513429346
Goodkat87 <ilyass.zrl@hotmail.com>
2024-02-20 13:47:16 +0100

    change banner size

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function Banner(props) {
     return (
-        <div className='w-full relative h-[800px] flex justify-center items-end max-sm:h-[450px]'>
+        <div className='w-full relative h-screen flex justify-center items-end max-sm:h-[450px]'>
                 <h1 className='text-7xl font-ahlan'>El Nabil</h1>
             <div className='w-full h-full absolute bg-cover bg-no-repeat bg-center -z-10 bg-[url("./assets/img/banner.jpg")] max-sm:bg-[-300px]'>
             </div>
