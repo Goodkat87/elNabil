1bad1550f8bcf683f7affcbd1b2d92a95f83626a
Goodkat87 <ilyass.zrl@hotmail.com>
2024-02-20 11:03:19 +0100

    Add cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import { useState } from 'react'
+import data from "./assets/json/data.json"
+import Card from './components/Card'
 
 function App() {
 
   return (
     <div className='w-full h-screen flex flex-col items-center'>
+      <div className='w-11/12 h-max flex flex-wrap justify-center gap-3'>
+        {data.map(function(item) {
+          return (
+            <Card item={item}></Card>
+          )
+        })}
+      </div>
     </div>
   )
 }
