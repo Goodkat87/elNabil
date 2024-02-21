42afe173a054b6f742808952b7dd9978d2b2d22e
Goodkat87 <ilyass.zrl@hotmail.com>
2024-02-21 16:12:58 +0100

    change url for image

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,73 @@
-import { useState, useEffect } from 'react';
-import data from "./assets/json/data.json"
+import { useState, useEffect } from "react";
+import data from "./assets/json/data.json";
 import Navbar from "./components/Navbar";
 import Imgbanner from "./assets/img/banner.jpg";
-import Card from './components/Card'
-import Footer from './components/Footer'
-import Copyright from './components/Copyright'
+import Card from "./components/Card";
+import Footer from "./components/Footer";
+import Copyright from "./components/Copyright";
 import Banner from "./components/Banner";
 import Bag from "./assets/img/bag.png";
-import SideBar from './components/SideBar';
+import SideBar from "./components/SideBar";
 
 function App() {
-  const [side, setSide] = useState(false)
-  const [basket, setBasket] = useState([]) 
-  const [solde, setSolde] = useState(300)
+  const [side, setSide] = useState(false);
+  const [basket, setBasket] = useState([]);
+  const [solde, setSolde] = useState(300);
 
-  const [added,setAdded] = useState(0)
-  const [cart,setCart] = useState([])
+  const [added, setAdded] = useState(0);
+  const [cart, setCart] = useState([]);
 
-  let add = (element)  => {
+  let add = (element) => {
     if (element.stock > 0 && solde > element.price) {
-      setBasket([...basket,element])
-      if (basket.length>0) {
-        setCart([...new Set(basket)])
-      }else{
-        setCart([...cart,element])
+      setBasket([...basket, element]);
+      if (basket.length > 0) {
+        setCart([...new Set(basket)]);
+      } else {
+        setCart([...cart, element]);
       }
-      setAdded(added+1)
-      element.stock--
-      element.quantity++
-      setSolde(solde-element.price)
+      setAdded(added + 1);
+      element.stock--;
+      element.quantity++;
+      setSolde(solde - element.price);
     }
-  }
+  };
 
-  
   let discard = (article) => {
-    if(article.quantity>0){
-      article.quantity --
-      article.stock ++
-      setSolde(solde+article.price)
-      setAdded(added-1)
+    if (article.quantity > 0) {
+      article.quantity--;
+      article.stock++;
+      setSolde(solde + article.price);
+      setAdded(added - 1);
     }
-  }
-
-
-  
+  };
 
   return (
-    <div className='w-full flex flex-col items-center '>
-      <SideBar side={side} solde={solde} basket={basket} cart={cart} add={add} discard={discard} ></SideBar>
-      <Navbar bag={Bag} side={side} setSide={setSide} basket={basket} added={added} />
+    <div className="w-full flex flex-col items-center ">
+      <SideBar
+        side={side}
+        solde={solde}
+        basket={basket}
+        cart={cart}
+        add={add}
+        discard={discard}
+      ></SideBar>
+      <Navbar
+        bag={Bag}
+        side={side}
+        setSide={setSide}
+        basket={basket}
+        added={added}
+      />
       <Banner imgbanner={Imgbanner} />
-      <div className='w-11/12 h-max flex flex-wrap justify-center gap-3 pb-32 pt-32'>
-        {data.map(function(item, key) {
-          return (
-            <Card item={item} key={key} add={add} added={added}  ></Card>
-          )
+      <div className="w-11/12 h-max flex flex-wrap justify-center gap-3 pb-32 pt-32">
+        {data.map(function (item, key) {
+          return <Card item={item} key={key} add={add} added={added}></Card>;
         })}
       </div>
       <Footer></Footer>
       <Copyright></Copyright>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 export default function Card(props) {
   return (
     <div className="group w-[30%] h-[30%] relative overflow-hidden gap-1 max-sm:w-36 max-sm:h-36">
-      <img src={props.item.image} alt="parfum" className='h-full w-full group-hover:scale-110 group-hover:transition group-hover:duration-[2s] group-hover:ease-in-out'/>
+      <img src={
+        new URL(`../assets/img/${props.item.image}`,import.meta.url).href
+      } alt="parfum" className='h-full w-full group-hover:scale-110 group-hover:transition group-hover:duration-[2s] group-hover:ease-in-out'/>
       <div className='flex flex-col gap-3 items-center justify-end pb-5 h-full w-full absolute z-10 bg-gradient-to-t from-black to-transparent group-hover:-translate-y-full group-hover:transition group-hover:duration-700 group-hover:ease-in-out'>
         
         <p className='max-sm:text-xs'>{props.item.name} - {props.item.price}€</p>
